8cafd8960324023783203f9b27a827e0b2ae8beb
sujal mallick <mallicksujal626@gmail.com>
2025-11-28 18:50:28 +0530

    custom Api response and error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv";
-import  connectDB  from "./db/conn.js";
+import connectDB from "./db/conn.js";
+import app from "./app.js";
 
 dotenv.config({
     path: './env'
 });
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        }
+        )
+    })
+    .catch((err) => {
+        console.log("DB Connection Error: ", err);
+    })
