092c437548f7e5e59bcdcf13f792f2c5f6e6ca44
sujal mallick <mallicksujal626@gmail.com>
2025-12-10 22:02:43 +0530

    refactor: format code for improved readability in subscription routes

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,14 +1,18 @@
-import { getSubscribedChannels, getUserChannelSubscribers, toggleSubscription } from "../controllers/subscription.controllers.js";
-import {verifyJWT} from "../middlewares/auth.middlewares.js";
+import {
+    getSubscribedChannels,
+    getUserChannelSubscribers,
+    toggleSubscription
+} from "../controllers/subscription.controllers.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 import { Router } from "express";
 
-const router=Router();
+const router = Router();
 
 
-router.route('/toggle/:channelId').post(verifyJWT,toggleSubscription);
+router.route('/toggle/:channelId').post(verifyJWT, toggleSubscription);
 
-router.route('/channel/:channelId').get(verifyJWT,getUserChannelSubscribers);
+router.route('/channel/:channelId').get(verifyJWT, getUserChannelSubscribers);
 
-router.route('/subscriber/:subscriberId').get(verifyJWT,getSubscribedChannels);
+router.route('/subscriber/:subscriberId').get(verifyJWT, getSubscribedChannels);
 
 export default router;
