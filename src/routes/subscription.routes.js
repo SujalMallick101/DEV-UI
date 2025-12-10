7018d46014aacb7eae2e782e1e276d85e3145c8c
sujal mallick <mallicksujal626@gmail.com>
2025-12-10 22:02:03 +0530

    feat: add subscription routes for managing user subscriptions including toggling, fetching subscribers, and subscribed channels

---
+++ src/routes/subscription.routes.js
@@ -0,0 +1,14 @@
+import { getSubscribedChannels, getUserChannelSubscribers, toggleSubscription } from "../controllers/subscription.controllers.js";
+import {verifyJWT} from "../middlewares/auth.middlewares.js";
+import { Router } from "express";
+
+const router=Router();
+
+
+router.route('/toggle/:channelId').post(verifyJWT,toggleSubscription);
+
+router.route('/channel/:channelId').get(verifyJWT,getUserChannelSubscribers);
+
+router.route('/subscriber/:subscriberId').get(verifyJWT,getSubscribedChannels);
+
+export default router;
